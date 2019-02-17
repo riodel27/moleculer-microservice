da85271c6305e4a3e90129c8a35c46afc68c6ae5
Rio <riopamaong@gmail.com>
2019-02-17 16:52:21 +0800

    init commit

---
+++ services/greeter.service.js
@@ -0,0 +1,81 @@
+"use strict";
+
+module.exports = {
+	name: "greeter",
+
+	/**
+	 * Service settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Service dependencies
+	 */
+	dependencies: [],	
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello'
+		 *
+		 * @returns
+		 */
+		hello() {
+			return "Hello Moleculer-microservice!!! - This is for the google container registry setup!!!";
+		},
+
+		/**
+		 * Welcome a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			params: {
+				name: "string"
+			},
+			handler(ctx) {
+				return `Welcome, ${ctx.params.name}`;
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+		console.log('service created greeter service')
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+		console.log('service started  greeter service')
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+		console.log('service stopped greeter service')
+	}
+};
