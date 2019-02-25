16079699ad4d97b9470b8465095afc41013fd0c5
Rio <riopamaong@gmail.com>
2019-02-25 16:22:03 +0800

    updated gitignore and some clean up

--- services/greeter.service.js
+++ services/greeter.service.js
@@ -26,7 +26,7 @@ module.exports = {
 		 * @returns
 		 */
 		hello() {
-			return "Hello Moleculer-microservice! This is dev environment!";
+			return "Hello Moleculer-microservice!";
 		},
 
 		/**
